0f0da1d7ad7ec28aa9239736bab1f3aaece9be8e
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-04-27 00:05:40 +0900

    Initialize project

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL('./index.html', import.meta.url)),
+      }
+    }
+  }
+})
