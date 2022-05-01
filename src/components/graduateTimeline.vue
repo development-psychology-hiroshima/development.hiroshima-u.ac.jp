84267881e92f83080dfbd8e8fc6832ecd080cf37
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-05-01 22:02:50 +0900

    finish index page

---
+++ src/components/graduateTimeline.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="graduate-timeline" v-for="graduateYear in graduates">
+    <h2 class="title-graduate-year">{{ graduateYear.year }}年度</h2>
+    <div class="graduate-details" v-for="(value, key) in graduateYear">
+      <div v-if="'year' !== key">{{ value }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "graduateTimeline",
+  inject: ["config"],
+  data() {
+    return {
+      graduates: this.config.graduates
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title-graduate-year {
+  background: none;
+}
+
+.graduate-timeline {
+  margin-bottom: 1rem;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import bannerCarouselWrapper from "./components/bannerCarouselWrapper.vue";
 import mobileMenuWrapper from "./components/mobileMenuWrapper.vue";
 import desktopMenuWrapper from "./components/desktopMenuWrapper.vue";
 import awardTimelineWrapper from "./components/awardTimelineWrapper.vue";
+import graduateTimeline from "./components/graduateTimeline.vue";
 
 const yaml = window.jsyaml;
 let config;
@@ -10,6 +11,7 @@ let config;
 const response = await fetch('config.yml')
   .then(response => response.text())
   .catch(error => undefined);
+
 try {
   config = yaml.load(response);
 } catch (e) {
@@ -20,3 +22,4 @@ createApp(bannerCarouselWrapper).provide("config", config).mount('#vue-carousel'
 createApp(mobileMenuWrapper).provide("config", config).mount('#mobile-menu');
 createApp(desktopMenuWrapper).provide("config", config).mount('#container-desktop-menu');
 createApp(awardTimelineWrapper).provide("config", config).mount('#container-awards');
+createApp(graduateTimeline).provide("config", config).mount('#container-graduate');
